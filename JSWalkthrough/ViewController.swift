95816796d79b3a43ad122c84ca140819e761b5c2
Joseph Stein <josephstein@me.com>
2015-08-23 20:49:23 -0700

    Initial Commit

---
+++ JSWalkthrough/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSWalkthrough
+//
+//  Created by Joseph Stein on 8/23/15.
+//  Copyright (c) 2015 Joseph Stein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
